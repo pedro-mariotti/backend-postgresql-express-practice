039c5dbcb19a01f93a58929b5e48c5c861b68e5b
Pedro Mariotti <57444804+pedro-mariotti@users.noreply.github.com>
2025-05-03 20:49:11 +0000

    Add pg dialect

--- api/database/db.config.js
+++ api/database/db.config.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
   dialect: "postgres",
+  dialectModule: pg,
   logging: false,
 });
 
